d070a93fd32a28416595538aa3be64d3e38d0000
MeiyappanKannappa <msmechatronics@gmail.com>
2017-10-12 15:46:46 -0700

    Hello World Ios

--- HelloProject/ViewController.swift
+++ HelloProject/ViewController.swift
@@ -6,10 +6,15 @@
 //  Copyright © 2017 Meiyappan Kannappa. All rights reserved.
 //
 
-import UIKit
+import UIKit // UI KIT is foundation library for all UI components from 
+//Apple. It has all fundamental UI components.
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textinput: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func doButtonClick(_ sender: Any) {
+        label.text = "Hello "+textinput.text!
+    }
 
 }
 
